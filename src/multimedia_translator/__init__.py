f916ce602249c39206629986fd296d6050b34e54
alanmehio <alan.mehio@gmail.com>
2025-08-21 20:52:32 +0200

    initial commit

---
+++ src/multimedia_translator/__init__.py
@@ -0,0 +1,15 @@
+"""
+
+| **Multimedia Translator**, a *Python Package*  which deals with different media translation from text, audio to video; one language to many languages
+| AI is used to make better translation accuracy 
+
+|
+
+"""
+
+__version__ = "1.0.0"
+
+__all__ = (
+    "__version__",
+    "main",
+)
---
+++ src/multimedia_translator/main.py
@@ -0,0 +1,61 @@
+'''
+| Make the project in edit mode
+
+>>>  pip install -e .
+
+>>> command here ????
+
+|
+'''
+import platform
+from argparse import ArgumentParser, Namespace
+
+def get_cli_value()->tuple[float,float,str]:
+   pass
+
+def main()-> None:
+     """
+    This is the main function that executes the program.
+    This function uses argparse to handle input from the command line.
+
+    Command-line arguments
+    ----------------------
+    -p : float
+        energy warning level; default to 50 dBm
+    -d : str
+        device name default to 'ttyACM0'
+
+    Examples:
+        >>> rfcentral -p 65.55 -d ttyACM0
+    """
+     parser = ArgumentParser(prog="rfcentral", usage="rfcentral -p 65.55 -d ttyACM0",description="RF Central Command Line Interface")
+
+
+     parser.add_argument(
+        '-p',
+        help='frequency engergy power level; if exceeded will give beep as warning',
+        type=float,
+        nargs='?',
+        default=50.00,
+        metavar='power'
+     )
+     parser.add_argument(
+        '-d',
+        help= 'device name  or RF device receiver name',
+        type= str,
+        default = 'ttyACM0',
+        nargs='?',
+        metavar = 'device'
+
+     )
+
+     args : Namespace = parser.parse_args()
+     power:float = args.p
+     device:str = args.d
+
+
+
+# this is important so that it does not run from pytest
+if __name__ == "__main__":
+    main()
+
