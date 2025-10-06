04e765958b27bf0de5eb777918737f09ad1fe605
Issam <issamhn2000@gmail.com>
2025-10-06 13:51:57 +0300

    tracing tox issue

--- tests/test_audio/test_server_audio_route.py
+++ tests/test_audio/test_server_audio_route.py
@@ -38,6 +38,7 @@ def test_audio_translate_valid():
     assert respone.status_code == 200
     data = respone.json()
     assert isinstance(data,list)
+    print(data[0])
     assert data[0]["text"] == "uno dos tres"
 
 
