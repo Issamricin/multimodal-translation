1e1a3c49a2f015b2ae36a589c67b73320e5e27a1
alanmehio <alan.mehio@gmail.com>
2025-12-02 20:13:42 +0100

    adding multi threading and return empty text for none supported text

--- src/multimodaltranslation/text/translate.py
+++ src/multimodaltranslation/text/translate.py
@@ -1,12 +1,17 @@
 import warnings
 
 from argostranslate import translate
+import logging
+import time
+import concurrent.futures
+
+logger = logging.getLogger(__name__)
 
 warnings.filterwarnings("ignore", category=FutureWarning,
                     module="stanza.models.tokenize.trainer")
 
 
-def translate_text(text:str, lang:str, targets:list) -> list:
+def translate_text(text:str, lang:str, targets:list[str]) -> list[dict[str,str]]:
     """
     Translates the text provided into the desired languages (targets).
 
@@ -17,20 +22,42 @@ def translate_text(text:str, lang:str, targets:list) -> list:
 
     Returns:
         list: List of translated texts with their target languages.
+              In case the language is not found , it will return empty text
     """
-    responses:list = []
+    t1 = time.perf_counter()
+    responses:list[dict[str,str]] = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=None) as executor:
+        for target in targets:
+            result = executor.submit(_do_translate,text, lang, target)
+            responses.append( result) 
+        data = concurrent.futures.as_completed(responses)
+        results:list[dict[str,str]] = []
+        for _ in concurrent.futures.as_completed(data):
+            results.append(_.result())
+        t2 = time.perf_counter()
+        delta = str(t2-t1)
+        logger.info(f'Transalation from {lang} to {targets} took {delta} seconds')
+        return results
+       
 
-    for target in targets:
 
-        try:
-            translation = translate.translate(text, lang, target)
-        except AttributeError:
-            responses.append({"Error": \
-            f"Either of the languages may not be available, {lang, target}." \
-            " Install the argos text-to-text translating language."})
+def _do_translate(text:str, lang:str, target:str)->dict[str,str]:
+      
+      try:
+        translated_text = translate.translate(text, lang, target)
+        return {"text": translated_text, "lang":target}
+      except AttributeError:
+        logger.warning( f"Either of the languages may not be available, {lang, target}." \
+         " Install the argos text-to-text translating language.")
+        return ({"text": "", "lang" : target})
+           
+      
 
-            continue
-
-        responses.append({"text": translation, "lang": target})
-
-    return responses
+if __name__ == "__main__":
+    lang = "en"
+    targets = ["dk","se", "ar", "ku"]
+    text = "Hi there"
+    results = translate_text(text=text, lang=lang, targets=targets)
+    for result in results:
+        print(f"{result['text']}  {result['lang']}")
+    
